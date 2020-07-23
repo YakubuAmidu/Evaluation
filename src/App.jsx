aae157ea218a357d4daba01203b4cc091974fbde
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-23 04:09:37 -0700

    Commiting changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-    <div>Evaluation, App component</div>
+    <div>
+   <div>Countdown to december 25, 2020</div>
+   <div>
+   <div>14 days</div>
+   <div>30 hours</div>
+   <div>15 minuites</div>
+   <div>20 seconds</div>
+   <input placeholder="New date" />
+   <button>Submit</button>
+   </div>
+    </div>
   )
   }
 }
