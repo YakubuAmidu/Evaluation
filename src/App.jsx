d3b221154f1c5c35d10c2f9100f780ce69336281
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-23 04:40:24 -0700

    Commiting changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+ constructor(props) {
+   super(props);
+   this.state = {
+     deadline: 'December 25, 2020'
+   }
+ }
+
   render() {
     return (
     <div className="App">
