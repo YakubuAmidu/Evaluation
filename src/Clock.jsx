c946388092e57534de3d89fa92e411839d2d9576
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-23 05:15:22 -0700

    Commiting changes

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -2,10 +2,25 @@ import React, { Componenet } from 'react';
 import './App.css';
 
 class Clock extends Componenet {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0;,
+      hours: 0,
+      minuites: 0,
+      seconds: 0
+    }
+  }
+
   render() {
     return (
-
-    )
+     <div>
+     <div className="clock-days">{this.state.days} days</div>
+     <div className="clock-hours">{this.state.hours} hours</div>
+     <div className="clock-minuites">{this.state.minuites} minuites</div>
+     <div className="clock-seconds">{this.state.seconds} seconds</div>
+     </div>
+   );
   }
 }
 
