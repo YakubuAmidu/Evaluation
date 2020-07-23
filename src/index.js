075f6164b0cc0516e5da57d0dd1cdeec0f2248ff
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-07-23 04:01:38 -0700

    Commiting changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App';
 
 ReactDom.render(
-  <div>Evaluation</div>, document.getElementById('root')
+  <App />, document.getElementById('root')
 );
